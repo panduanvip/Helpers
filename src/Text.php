405f793131929af99e21e131e3d9be272745ba83
Panduan.VIP <webpanduanvip@gmail.com>
2021-05-16 14:52:58 +0700

    slug function
    add a slug function

--- src/Text.php
+++ src/Text.php
@@ -79,9 +79,9 @@ class Text{
     }
 
 
-    /*----------------------------------------------------------------
+    /*-----------------------------------------------------------------------
         Get string between two strings
-    ----------------------------------------------------------------*/
+    -----------------------------------------------------------------------*/
 
     public static function string_between($string, $start, $end){
         $string = ' ' . $string;
@@ -91,6 +91,132 @@ class Text{
         $ini += strlen($start);
         $len = strpos($string, $end, $ini) - $ini;
         return substr($string, $ini, $len);
+    }
+
+
+    
+    /*-----------------------------------------------------------------------
+        Sanitize title with dash for creating url slug
+        This code is taken from wordpress in wp-includes/formatting.php
+        with minimum modifications
+    -----------------------------------------------------------------------*/
+
+    public static function slug($title)
+    {
+        $title = strip_tags($title);
+        $title = preg_replace('|%([a-fA-F0-9][a-fA-F0-9])|', '---$1---', $title);
+        $title = str_replace('%', '', $title);
+        $title = preg_replace('|---([a-fA-F0-9][a-fA-F0-9])---|', '%$1', $title);
+
+        if (self::seems_utf8($title)) {
+            if (function_exists('mb_strtolower')) {
+                $title = mb_strtolower($title, 'UTF-8');
+            }
+            $title = self::utf8_uri_encode($title, 200);
+        }
+        $title = strtolower($title);
+        $title = preg_replace('/&.+?;/', '', $title);
+        $title = str_replace('.', '-', $title);
+        $title = preg_replace('/[^%a-z0-9 _-]/', '', $title);
+        $title = preg_replace('/\s+/', '-', $title);
+        $title = preg_replace('|-+|', '-', $title);
+        $title = trim($title, '-');
+        return urldecode($title);
+    }
+
+    private static function seems_utf8($str)
+    {
+        self::mbstring_binary_safe_encoding();
+        $length = strlen($str);
+        self::reset_mbstring_encoding();
+        for ($i = 0; $i < $length; $i++) {
+            $c = ord($str[$i]);
+            if ($c < 0x80) $n = 0; # 0bbbbbbb
+            elseif (($c & 0xE0) == 0xC0) $n = 1; # 110bbbbb
+            elseif (($c & 0xF0) == 0xE0) $n = 2; # 1110bbbb
+            elseif (($c & 0xF8) == 0xF0) $n = 3; # 11110bbb
+            elseif (($c & 0xFC) == 0xF8) $n = 4; # 111110bb
+            elseif (($c & 0xFE) == 0xFC) $n = 5; # 1111110b
+            else return false; # Does not match any model
+            for ($j = 0; $j < $n; $j++) { # n bytes matching 10bbbbbb follow ?
+                if ((++$i == $length) || ((ord($str[$i]) & 0xC0) != 0x80))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private static function utf8_uri_encode($utf8_string, $length = 0)
+    {
+        $unicode = '';
+        $values = array();
+        $num_octets = 1;
+        $unicode_length = 0;
+
+        self::mbstring_binary_safe_encoding();
+        $string_length = strlen($utf8_string);
+        self::reset_mbstring_encoding();
+
+        for ($i = 0; $i < $string_length; $i++) {
+
+            $value = ord($utf8_string[$i]);
+
+            if ($value < 128) {
+                if ($length && ($unicode_length >= $length))
+                    break;
+                $unicode .= chr($value);
+                $unicode_length++;
+            } else {
+                if (count($values) == 0) $num_octets = ($value < 224) ? 2 : 3;
+
+                $values[] = $value;
+
+                if ($length && ($unicode_length + ($num_octets * 3)) > $length)
+                    break;
+                if (count($values) == $num_octets) {
+                    if ($num_octets == 3) {
+                        $unicode .= '%' . dechex($values[0]) . '%' . dechex($values[1]) . '%' . dechex($values[2]);
+                        $unicode_length += 9;
+                    } else {
+                        $unicode .= '%' . dechex($values[0]) . '%' . dechex($values[1]);
+                        $unicode_length += 6;
+                    }
+
+                    $values = array();
+                    $num_octets = 1;
+                }
+            }
+        }
+
+        return $unicode;
+    }
+
+    private static function mbstring_binary_safe_encoding($reset = false)
+    {
+        static $encodings = array();
+        static $overloaded = null;
+
+        if (is_null($overloaded))
+            $overloaded = function_exists('mb_internal_encoding') && (ini_get('mbstring.func_overload') & 2);
+
+        if (false === $overloaded)
+            return;
+
+        if (!$reset) {
+            $encoding = mb_internal_encoding();
+            array_push($encodings, $encoding);
+            mb_internal_encoding('ISO-8859-1');
+        }
+
+        if ($reset && $encodings) {
+            $encoding = array_pop($encodings);
+            mb_internal_encoding($encoding);
+        }
+    }
+
+    private static function reset_mbstring_encoding()
+    {
+        self::mbstring_binary_safe_encoding(true);
     }
 
 }
--- test.php
+++ test.php
@@ -1,47 +1,19 @@
-<style>
-    hr {
-        border: none;
-        height: 1px;
-        /* Set the hr color */
-        color: #333; /* old IE */
-        background-color: #333; /* Modern Browsers */
-    }
-</style>
-
 <?php
 
 include 'vendor/autoload.php';
 
 use PanduanVIP\TextManipulation\Text;
 
-echo '<h1>PanduanVIP Text Library</h1>';
-echo '<hr>';
-
-echo '<h2>How To Use?</h2>';
-echo '<pre>include "vendor/autoload.php";<br>';
-echo 'use PanduanVIP\Text\Text;</pre>';
-echo '<hr>';
-
 
 // spintax
 
-echo '<h2>Text::spintax ( $string ) : string</h2>';
-
-echo '<pre>$result = Text::spintax($string);</pre><br>';
-
 $string = "{green|blue|yellow} bird is sitting {there|over there|on the ground}.";
-echo "<b>string:</b> $string<br>";
-
-$sentence = Text::spintax($string);
-echo "<b>result:</b> $sentence";
+$result = Text::spintax($string);
+echo $result;
 echo '<hr>';
 
 
 // string_to_array
-
-echo '<h2>Text::string_to_array ( $string ) : array</h2>';
-
-echo '<pre>$result = Text::string_to_array($string);</pre><br>';
 
 $string = <<<test
 Example of string
@@ -49,58 +21,39 @@ spanning multiple lines
 using heredoc syntax.
 test;
 
-echo "<b>string:</b>";
-echo "<pre>";
-echo $string;
 $result = Text::string_to_array($string);
-echo '</pre>';
-echo "<b>result:</b>";
-echo '<pre>';
 print_r($result);
-echo "</pre>";
 echo '<hr>';
 
 
 // excerpt
 
-echo '<h2>Text::excerpt ( $string, $max_length=40, $cut_off="...", $keep_word=true ) : string</h2>';
-
-echo '<pre>$result = Text::excerpt($string, 50);</pre><br>';
-
 $string = "PHP is a general-purpose scripting language especially suited to web development. It was stringly created by Danish-Canadian programmer Rasmus Lerdorf in 1994. The PHP reference implementation is now produced by The PHP Group.";
-echo "<b>string:</b> $string<br>";
-$excerpt = Text::excerpt($string, 50);
-echo "<b>result:</b> $excerpt";
+$result = Text::excerpt($string, 50);
+echo $result;
 echo '<hr>';
 
 
 // remove_double_space
 
-echo '<h2>Text::remove_double_space ( $string ) : string</h2>';
-
-echo '<pre>$result = Text::remove_double_space($string);</pre><br>';
-
 $string = "Lorem     ipsum dolor sit amet,   consectetur adipiscing elit";
-echo "<b>string:</b>";
-echo "<pre>";
-echo $string;
 $result = Text::remove_double_space($string);
-echo '</pre>';
-echo "<b>result:</b>";
-echo '<pre>';
-echo($result);
-echo "</pre>";
+echo $result;
 echo '<hr>';
 
 
 // string_between
 
-echo '<h2>Text::string_between ( $string, $start, $end ) : string</h2>';
-
-echo '<pre>$result = Text::string_between($string, "my", "!");</pre><br>';
-
 $string = "This is my dog!";
-echo "<b>string:</b> $string<br>";
 $result = Text::string_between($string, "my", "!");
-echo "<b>result:</b> $result";
+echo $result;
 echo '<hr>';
+
+
+// slug
+
+$string = "красивые сакуры";
+$result = Text::slug($string);
+echo $result;
+echo '<hr>';
+
